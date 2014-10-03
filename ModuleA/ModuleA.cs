e89a0ac2ab884bd4959d5e80181067265e4c6c7d
xorch <italy21>
2014-10-02 20:32:47 -0700

    2

---
+++ ModuleA/ModuleA.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuleA
+{
+    public class ModuleA : IModule
+    {
+        protected IRegionManager RegionManager { get; private set; }
+
+        public ModuleA(IRegionManager regionManager)
+        {
+            RegionManager = regionManager;
+        }
+
+        public void Initialize()
+        {
+            RegionManager.RegisterViewWithRegion("MainRegion", typeof(ModuleAView));
+        }
+    }
+}
---
+++ Shell/Bootstrapper.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.UnityExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Shell
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return new ShellWindow();
+        }
+
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+
+            App.Current.MainWindow = (Window)this.Shell;
+            App.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureModuleCatalog()
+        {
+            base.ConfigureModuleCatalog();
+
+            ModuleCatalog moduleCatalog = (ModuleCatalog)this.ModuleCatalog;
+
+            moduleCatalog.AddModule(typeof(ModuleA.ModuleA), InitializationMode.WhenAvailable);
+        }
+    }
+}
